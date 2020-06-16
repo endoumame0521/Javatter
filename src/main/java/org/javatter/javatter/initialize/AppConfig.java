5252ee65b69d15113bd14da80168b358eb2e6577
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-06-16 19:29:57 +0900

    Add: add framework of ModelMapper

---
+++ src/main/java/org/javatter/javatter/initialize/AppConfig.java
@@ -0,0 +1,21 @@
+package org.javatter.javatter.initialize;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// Springにbeanとして登録して@Autowiredで使用できるようにする
+// 同時にModelMapperの初期設定を定義
+@Configuration
+@ComponentScan("org.javatter")
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        // コピー元がnullのカラムはコピー先に上書きしない設定を有効に
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        return modelMapper;
+    }
+}
