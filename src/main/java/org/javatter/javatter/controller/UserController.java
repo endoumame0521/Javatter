ac06b1b25fa06850f69963c9a3b223c62c8c90f5
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-06-21 23:41:30 +0900

    Update: support for interceptor

--- src/main/java/org/javatter/javatter/controller/UserController.java
+++ src/main/java/org/javatter/javatter/controller/UserController.java
@@ -2,12 +2,15 @@ package org.javatter.javatter.controller;
 
 import java.util.List;
 
+import org.javatter.javatter.annotation.ExcludeInterceptor;
 import org.javatter.javatter.converter.UserConverter;
 import org.javatter.javatter.entity.User;
 import org.javatter.javatter.form.UserForm;
 import org.javatter.javatter.form.UserUpdateForm;
 import org.javatter.javatter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,8 +20,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -26,23 +31,24 @@ public class UserController {
     @Autowired
     private UserConverter userConverter;
 
-    @GetMapping("/users")
+    @ExcludeInterceptor
+    @GetMapping("")
     public String index(Model model) {
         List<User> users = userService.getUsers();
-        User currentUser = userService.getCurrentUser();
         model.addAttribute("users", users);
-        model.addAttribute("currentUser", currentUser);
         return "users/index";
     }
 
-    @GetMapping("/users/new")
+    @ExcludeInterceptor
+    @GetMapping("/new")
     public String newUser(Model model) {
         UserForm userForm = new UserForm();
         model.addAttribute("userForm", userForm);
         return "users/new";
     }
 
-    @PostMapping("/users")
+    @ExcludeInterceptor
+    @PostMapping("")
     public String create(@Validated UserForm userForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "users/new";
@@ -51,15 +57,16 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @GetMapping("/users/{id}")
+    @ExcludeInterceptor
+    @GetMapping("/{id}")
     public String show(@PathVariable Long id, Model model) {
         User user = userService.getUser(id);
         model.addAttribute("user", user);
         return "users/show";
     }
 
-    @GetMapping("/users/{id}/edit")
-    public String edit(@PathVariable Long id, Model model) {
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails, Model model) {
         User user = userService.getUser(id);
 
         UserUpdateForm userUpdateForm = new UserUpdateForm();
@@ -69,7 +76,7 @@ public class UserController {
         return "users/edit";
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/{id}")
     public String update(@PathVariable Long id, @Validated UserUpdateForm userUpdateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "users/edit";
@@ -78,7 +85,7 @@ public class UserController {
         return String.format("redirect:/users/%d", id);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public String destroy(@PathVariable Long id) {
         userService.deleteUser(id);
         return "redirect:/users";
